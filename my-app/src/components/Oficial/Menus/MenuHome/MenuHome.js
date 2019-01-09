462a42c70cd49c15e4650685ca8a2c80161fd266
pablomumar <pablomumar@hotmail.com>
2019-01-09 19:31:08 +0100

    Routes (react-router-dom)

---
+++ my-app/src/components/Oficial/Menus/MenuHome/MenuHome.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MenuHome extends Component{
+    render(){
+        return (
+            <h1>MenuHome</h1>
+        )
+    }
+}
+
+export default MenuHome;
