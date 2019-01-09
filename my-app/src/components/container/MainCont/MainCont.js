917bc590a2fb4800a6b2b0cf868e6c73c2bffff8
pablomumar <pablomumar@hotmail.com>
2019-01-09 19:31:57 +0100

    Routes (react-router-dom)

--- my-app/src/components/container/MainCont/MainCont.js
+++ my-app/src/components/container/MainCont/MainCont.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import  {Route} from 'react-router-dom';
+
 import Auxiliar from '../../hoc/Auxiliar/Auxilar';
 import classes from './MainCont.module.css';
+import MenuHome from '../../../components/Oficial/Menus/MenuHome/MenuHome';
+import Colecciones from '../../../components/Oficial/Pages/Colecciones/Colecciones';
+import Bio from '../../../components/Oficial/Pages/Bio/Bio';
+import Redes from '../../../components/Oficial/Pages/Redes/Redes';
 
 class MainCont extends Component {
     render() {
         return (
             <Auxiliar>
-                <div className={[classes.mainFrame, classes.paddingLatNVert, classes.fondoBlack].join(' ')}>
-                    <h1 style={{color: 'white'}}>MainCont</h1>
-                </div>
+                <Route path='/' exact render={() => (
+                    <Auxiliar>
+                        <div className={[classes.mainFrame, classes.paddingLatNVert, classes.fondoWhite].join(' ')}>
+                            <MenuHome/>
+                        </div>
+                    </Auxiliar>
+                )}/>
+               <Route path='/Colecciones' exact component={Colecciones}/>
+               <Route path='/Bio' exact component={Bio}/>
+               <Route path='/Redes' exact component={Redes}/>
             </Auxiliar>
         );
     }
