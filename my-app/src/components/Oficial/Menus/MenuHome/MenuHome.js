3a0b26a569f3e12f45aa208788068f1217661702
pablomumar <pablomumar@hotmail.com>
2019-01-12 19:36:30 +0100

    menuHome v0.2 w/ design test include

--- my-app/src/components/Oficial/Menus/MenuHome/MenuHome.js
+++ my-app/src/components/Oficial/Menus/MenuHome/MenuHome.js
@@ -5,10 +5,20 @@ import classes from './MenuHome.module.css';
 import LinksHome from './LinksHome/LinksHome';
 import Auxiliar from '../../../hoc/Auxiliar/Auxilar';
 
+import logo from '../../../../assets/logos/cuadraoSimple.png'
+
 class MenuHome extends Component{
     render(){
         return (
             <Auxiliar>
+                <div className={classes.contFixedLogo}>
+                    <div>
+                        <img src={logo} alt=""/>
+                    </div>
+                    <div className={classes.extraPrueba}> </div>
+                    <div className={[classes.extraPrueba, classes.giroPrueba].join(' ')}> </div>
+                    <div className={classes.fondoBlanco}> </div>
+                </div>
                 <div className='container-fluid'> {/* Meter logo y animaciones en fixed*/}
                     <div className={[classes.mainCont, 'row'].join(' ')}>
                         <LinksHome topPerc={40} leftPerc={40} transTop={-50} transLeft={-50} nombreComponent='/redes'>Contacto</LinksHome>
